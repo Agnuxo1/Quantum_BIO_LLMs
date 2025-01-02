4168f8a01225b5c05551624e367cd9b458aa7e7c
Francisco Angulo de Lafuente <166046035+Agnuxo1@users.noreply.github.com>
2025-01-02 11:16:57 +0100

    Add files via upload
    Signed-off-by: Francisco Angulo de Lafuente <166046035+Agnuxo1@users.noreply.github.com>

---
+++ JavaScript_DEMO/app/page.tsx
@@ -0,0 +1,10 @@
+import { HolographicDisplay } from '@/components/holographic-display'
+
+export default function Home() {
+  return (
+    <div className="container mx-auto py-6">
+      <HolographicDisplay />
+    </div>
+  )
+}
+
